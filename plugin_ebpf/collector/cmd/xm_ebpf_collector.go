a22b8ff25683669faeb8715087873d9a0e58fffb
calmwu <wubo0067@hotmail.com>
2023-02-06 17:37:59 +0800

    feat:start coding xm ebpf collector

---
+++ plugin_ebpf/collector/cmd/xm_ebpf_collector.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: CALM.WU
+ * @Date: 2023-02-06 11:39:12
+ * @Last Modified by: CALM.WU
+ * @Last Modified time: 2023-02-06 16:53:30
+ */
+
+package cmd
+
+func Main(release, commit, builtTime string) {
+}
--- plugin_ebpf/collector/main.go
+++ plugin_ebpf/collector/main.go
@@ -7,6 +7,18 @@
 
 package main
 
-func main() {
+import (
+	_ "go.uber.org/automaxprocs"
+)
 
+var (
+	// Release thor-med release version
+	Release string
+	// Commit git commit id
+	Commit string
+	// BuildTime thor-med Binary compilation time
+	BuildTime string
+)
+
+func main() {
 }
