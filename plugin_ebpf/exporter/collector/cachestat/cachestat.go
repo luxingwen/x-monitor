b6a769910c3be144716dc348d01e23d5ad2cc488
calmwu <wubo0067@hotmail.com>
2023-02-16 17:38:04 +0800

    feat:start implement cachestat collector

--- plugin_ebpf/exporter/collector/cachestat/cachestat.go
+++ plugin_ebpf/exporter/collector/cachestat/cachestat.go
@@ -7,4 +7,23 @@
 
 package cachestat
 
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"xmonitor.calmwu/plugin_ebpf/exporter/collector"
+)
+
 //go:generate env GOPACKAGE=cachestat bpf2go -no-strip -cc clang -type cachestat_value XMCacheStat ../../../bpf/xm_cachestat.bpf.c -- -I../../../ -I../../../bpf/.output -I../../../../extra/include/bpf -g -Wall -Werror -D__TARGET_ARCH_x86 -DOUTPUT_SKB
+
+func init() {
+	collector.RegisterEBPFModule("cachestat", NewCacheStatCollector)
+}
+
+type cacheStatCollector struct{}
+
+func NewCacheStatCollector() (collector.EBPFModule, error) {
+	return nil, nil
+}
+
+func (cs *cacheStatCollector) Update(ch chan<- prometheus.Metric) error {
+	return nil
+}
