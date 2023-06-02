5338c8621449f123b4fe22982a71a025df1c7d60
calm.wu <wubo0067@hotmail.com>
2023-06-02 18:37:52 +0800

    feat:oom test

--- tools/bin/cg_pressure.sh
+++ tools/bin/cg_pressure.sh
@@ -3,4 +3,4 @@
 # --vm 1个进程
 # --vm-bytes 分配500m内存
 # --vm-keep 一直占用内存
-cgexec -g memory,cpu:/hello-cg stress-ng --vm 1 --vm-bytes 949450000 --vm-keep --timeout 100s --verbose
+cgexec -g cpu,memory:/user.slice/hello-cg stress-ng --vm 1 --vm-bytes 100M --vm-hang 0 --vm-keep --timeout 100s --iterations 11 --verbose
