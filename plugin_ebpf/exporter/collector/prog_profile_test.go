f1d26c79e5f55de252851d150c428bb3a9044e78
calm.wu <wubo0067@hotmail.com>
2023-08-18 18:27:16 +0800

    feat:add test

--- plugin_ebpf/exporter/collector/prog_profile_test.go
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: CALM.WU
- * @Date: 2023-08-18 16:18:10
- * @Last Modified by: CALM.WU
- * @Last Modified time: 2023-08-18 16:50:48
- */
-
-package collector
---
+++ plugin_ebpf/exporter/internal/bpf_prog/perf_event_test.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: CALM.WU
+ * @Date: 2023-08-18 17:24:20
+ * @Last Modified by: CALM.WU
+ * @Last Modified time: 2023-08-18 17:24:48
+ */
+
+package bpfprog
+
+import "testing"
+
+func TestPerfEventProg(t *testing.T) {
+
+}
